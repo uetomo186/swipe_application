eb43b8b1611e5011f605aa89187e1518815e1790
tomohiro ueno <hagurono9@gmail.com>
2024-01-17 01:35:15 +0900

    add

---
+++ lib/screens/profile/views/profile_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:swipe_application/blocs/sign_in_bloc/sign_in_bloc.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome, you are In !'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                context.read<SignInBloc>().add(const SignOutRequired());
+              },
+              icon: const Icon(Icons.login))
+        ],
+      ),
+    );
+  }
+}
