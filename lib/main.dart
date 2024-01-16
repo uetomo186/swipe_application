f5ababd3a5ec0f48f0be1ef3b1130a7327b3cf99
tomohiro ueno <hagurono9@gmail.com>
2024-01-17 00:41:28 +0900

    firebaseの設定を追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,23 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:swipe_application/app.dart';
+import 'package:swipe_application/firebase_options.dart';
 import 'package:user_repository/user_repository.dart';
 
 import 'simple_bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = SimpleBlocObserver();
   runApp(MyApp(FirebaseUserRepo()));
+
+  try {
+    await Firebase.initializeApp();
+  } catch (e) {
+    print('Firebase initialization failed: $e');
+    // その他のエラーハンドリングをここに追加
+  }
 }
