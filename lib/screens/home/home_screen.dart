e5a80dbac6cf8552d94aa008f7621d49925c5196
tomohiro ueno <hagurono9@gmail.com>
2024-01-17 18:19:53 +0900

    ホーム画面アクションを追加

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,25 +1,46 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:swipe_application/blocs/sign_in_bloc/sign_in_bloc.dart';
+import 'package:swipe_cards/draggable_card.dart';
+import 'package:swipe_cards/swipe_cards.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _controller = PageController(initialPage: 0);
+
+  int numberPhotos = 4;
+  int currentPage = 0;
+
+  List<SwipeItem> items = [
+    SwipeItem(
+        content: "Liz",
+        likeAction: () {
+          log("Liked");
+        },
+        nopeAction: () {
+          log("Nope");
+        },
+        superlikeAction: () {
+          log("Super Liked");
+        },
+        onSlideUpdate: (SlideRegion? region) async {
+          log("Region $region");
+        }),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        title: const Text('Welcome, you are In !'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                context.read<SignInBloc>().add(const SignOutRequired());
-              },
-              icon: const Icon(Icons.login))
-        ],
-      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
     );
   }
 }
