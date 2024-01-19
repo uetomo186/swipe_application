f23c19d9dcc72c18615de5735693a3f329c4cafb
tomohiro ueno <hagurono9@gmail.com>
2024-01-19 17:17:29 +0900

    photo

--- lib/screens/profile/views/profile_screen.dart
+++ lib/screens/profile/views/profile_screen.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:swipe_application/blocs/sign_in_bloc/sign_in_bloc.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  List<String> newPhotos = [];
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +30,51 @@ class ProfileScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Text(
+                  'Photos',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: 6,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10,
+                              childAspectRatio: 9 / 16),
+                      itemBuilder: (context, i) {
+                        return GestureDetector(
+                          onTap: () {
+                            if (!(newPhotos.contains(i.toString()))) {
+                              setState(() {
+                                newPhotos.add(i.toString());
+                              });
+                            } else {
+                              setState(() {
+                                newPhotos.remove(i.toString());
+                              });
+                            }
+                          },
+                        );
+                      }),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
